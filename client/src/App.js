16dede002349fb9ae1efaddd760e048b82e25b0d
Sebastian Mazgaj <sebastian.mazgaj189@gmail.com>
2024-11-22 21:58:51 +0100

    change authSlice to check the jwt access token and add protected route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { jwtDecode } from 'jwt-decode';
+import { logout } from './store/authSlice';
 import store from './store';
+import { Provider } from 'react-redux';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
@@ -11,32 +14,58 @@ import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
 import RecipeDetails from './pages/RecipeDetails/RecipeDetails';
 import RecipesByCategory from './pages/RecipesByCategory/RecipesByCategory';
-const Recipes = lazy(() => import('./pages/Recipes/Recipes'))
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+
+const Recipes = lazy(() => import('./pages/Recipes/Recipes'));
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+
+    if (token) {
+      const decoded = jwtDecode(token);
+      if (decoded.exp * 1000 <= Date.now()) {
+        dispatch(logout());
+      }
+    }
+  }, [dispatch]);
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/recipes" element={<Recipes />} />
-              <Route path="/recipes/:id" element={<RecipeDetails />} />
-              <Route path="/recipes/category/:categoryId" element={<RecipesByCategory />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </Provider>
-    </Suspense>
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+          <Route path="/recipes/category/:categoryId" element={<RecipesByCategory />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
-export default App;
+export default function WrappedApp() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <App />
+        </Suspense>
+      </Router>
+    </Provider>
+  );
+}
